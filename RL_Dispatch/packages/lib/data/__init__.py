9ab0b8140b7a6f12d6dad0a2b2895d1bf2680333
364271387@qq.com <930824>
2020-11-24 14:58:28 +0800

    new

--- RL_Dispatch/packages/lib/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import loader
-from . import sample_transform
-from .loader import *
-from .sample_transform import *
--- RL_Dispatch/packages/lib/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import load_config
-from . import yaml_helper
-from . import replay_buffer
-from .load_config import *
-from .yaml_helper import *
